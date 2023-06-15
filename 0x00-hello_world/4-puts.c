94a9a44da8bbf5d43d7c7ffebae1f277c573985f
ALXNimrodSekwati <sekwatinimrod22@gmail.com>
2023-06-15 11:32:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - A program that use a puts function 
- *
- * Return: Always 0 (Success)
+ * main - program that uses Puts Function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
