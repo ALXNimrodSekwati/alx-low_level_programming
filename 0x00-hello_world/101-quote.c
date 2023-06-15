ec959ce3aadae65b2951ce145a4637ccbdb59042
ALXNimrodSekwati <sekwatinimrod22@gmail.com>
2023-06-15 12:11:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
-* main - 101-quote
+* main - Entry point
 *
 * Return: Always 1 (Success)
 */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
